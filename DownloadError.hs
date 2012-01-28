f36bb8000c2d87f0f02cc95e4220d3ce4523b65e
Thorsten Lorenz <thlorenz@gmx.de>
2012-01-27 20:41:59 -0500

    improved error handling to use Maybe, but need to implement exception handker

--- DownloadError.hs
+++ DownloadError.hs
@@ -2,14 +2,30 @@ import Network.HTTP.Conduit
 import qualified Data.ByteString.Lazy as L
 import Control.Exception as X
 
-main = do
-    let badUrl = "http://www.google.com/intl/en_com/images/srpr/WRONG.png"    
+badUrl = "http://www.google.com/intl/en_com/images/srpr/WRONG.png"    
+
+oldmain = do
     imgData <- (simpleHttp badUrl) `X.catch` statusExceptionHandler  
     case imgData of x | x == L.empty -> return () 
                       | otherwise    -> L.writeFile "my.png" imgData
 
-statusExceptionHandler ::  HttpException -> IO L.ByteString
-statusExceptionHandler (StatusCodeException status headers) = 
-    putStr "An error occured during download: "
-    >> (putStrLn $ show status)
-    >> (return L.empty)
+    where
+        statusExceptionHandler ::  HttpException -> IO L.ByteString
+        statusExceptionHandler (StatusCodeException status headers) = 
+            putStr "An error occured during download: "
+            >> (putStrLn $ show status)
+            >> (return L.empty)
+
+main = do
+    let imgData = getImgData
+    case imgData of  Just x  -> x >>= L.writeFile "test.png"
+                     Nothing -> return ()
+    where
+        getImgData :: Maybe (IO L.ByteString)
+        getImgData =  (Just $ simpleHttp badUrl) -- `X.catch` statusExceptionHandler 
+
+        statusExceptionHandler :: HttpException -> Maybe (IO L.ByteString)
+        statusExceptionHandler (StatusCodeException status headers) = Nothing
+    
+
+
