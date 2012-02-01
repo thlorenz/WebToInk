6b2c76849c72fbb89b572b5531bcf4318699501c
Thorsten Lorenz <thlorenz@gmx.de>
2012-01-31 22:35:28 -0500

    adding make data list

---
+++ make-datalist.sh
@@ -0,0 +1,6 @@
+rm -rf Data\ List
+runghc HtmlBookToMobi.hs \
+    --title "Data List" \
+    --language "en-us" \
+    --author "hackage" \
+    --toc "http://hackage.haskell.org/packages/archive/base/latest/doc/html/Data-List.html" \
