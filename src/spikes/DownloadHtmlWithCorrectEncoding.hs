555a714b75ba7c2b6a9aa225a3eefeac87b6e43f
Thorsten Lorenz <thlorenz@gmx.de>
2012-02-06 23:12:59 -0500

    working func with string extraction

--- src/spikes/DownloadHtmlWithCorrectEncoding.hs
+++ src/spikes/DownloadHtmlWithCorrectEncoding.hs
@@ -1,9 +1,13 @@
 import Network.HTTP.Enumerator (simpleHttp)
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.UTF8 as U
+import Control.Monad.IO.Class (MonadIO)
 
-openUrl = simpleHttp
+openUrl :: String -> IO String
+openUrl url = do
+   bs <- simpleHttp url
+   return $ U.toString bs
 
 main = do
     bs <- openUrl "http://blog.bjrn.se/2008/10/lets-build-mp3-decoder.html" 
-    putStrLn $ U.toString bs
+    putStrLn bs
