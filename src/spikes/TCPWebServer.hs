7a4dca71ef056448356fa7a939cbb6eb5c627d27
Thorsten Lorenz <thlorenz@gmx.de>
2012-03-18 15:02:06 -0400

    webserver working (always serves same html at this point)

--- src/spikes/TCPWebServer.hs
+++ src/spikes/TCPWebServer.hs
@@ -1,46 +1,73 @@
 import Network (listenOn, accept, PortID(..), Socket)
 import System (getArgs)
-import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
+import System.IO
 import Control.Concurrent (forkIO)
 
 main :: IO ()
 main = do
     args <- getArgs
-    let port = fromIntegral (read $ head args :: Int)
+    let port = fromIntegral 3000 -- fromIntegral (read $ head args :: Int)
     sock <- listenOn $ PortNumber port
-    putStrLn $ "Listening on " ++ (head args)
-    sockHandler sock
+    putStrLn $ "Listening on " ++ (show port)
+    sockHandler sock 0
 
-sockHandler :: Socket -> IO ()
-sockHandler sock = do
+sockHandler :: Socket -> Int -> IO ()
+sockHandler sock sessionId = do
     (handle, host, port) <- accept sock
     putStrLn $ "Accepted " ++ (show host) ++ ":" ++ (show port)
    
     -- no buffering for client's socket handle
     hSetBuffering handle NoBuffering
 
-    forkIO $ commandProcessor handle
+    forkIO $ requestProcessor handle sessionId
 
     -- handle more incoming connections
-    sockHandler sock
+    sockHandler sock (sessionId + 1)
 
-commandProcessor :: Handle -> IO () 
-commandProcessor handle = do
-    line <- hGetLine handle
-    let cmd = words line
-    case (head cmd) of
-        "echo"  -> echoCommand handle cmd
-        "add"   -> addCommand handle cmd
-        _       -> hPutStrLn handle "Unknown command"
+requestProcessor :: Handle -> Int -> IO ()
+requestProcessor handle sessionId = do
+
+    reqString <- hGetLine handle
+    putStrLn $ "client: [" ++ (show $ length reqString) ++ "] " ++ reqString
     
-    -- recurse to execute several commands over same connection
-    commandProcessor handle
+    -- Empty line in request denotes that it is complete
+    if (length reqString <= 1 )
+        then do
+            -- Response Header
+            hPutStrLn handle header
+            hPutStrLn handle ""
 
-echoCommand :: Handle -> [String] -> IO ()
-echoCommand handle cmd = do
-    hPutStrLn handle (unwords $ tail cmd)
+            -- Response Body
+            hPutStrLn handle body
 
-addCommand :: Handle -> [String] -> IO ()
-addCommand handle cmd = do
-    hPutStrLn handle $ show $ (read $ cmd !! 1) + (read $ cmd !! 2)
+            -- Send all and close connection
+            hFlush handle
+            hClose handle
+
+            print "DONE"
+        else 
+            requestProcessor handle sessionId
+
+  where 
+    status        =  "HTTP/1.1 200 OK"
+    server        =  "MyHaskellServer/0.0.1 (Darwin)"
+    lastModified  =  "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT"
+    etag          =  "Etag: \"3f80f-1b6-3e1cb03b\""
+    acceptRanges  =  "Accept-Ranges: bytes"
+    -- length of body + <CR><LF>
+    contentLength =  "Content-Length: " ++ (show $ (length body + 2))
+    connection    =  "Connection: close"
+    contentType   =  "Content-Type: text/html; charset=UTF-8"
+
+    body = "<body><h1>Hello World</h1></body>" 
+    header = unlines $
+        [ status
+        , server
+        , lastModified
+        , etag
+        , acceptRanges
+        , contentLength
+        , connection
+        , contentType
+        ]
 
