a59c54a4dd088c2fe129c8d38191d7816d63679c
Thorsten Lorenz <thlorenz@gmx.de>
2012-03-19 07:29:20 -0400

    serving html and stylesheet

--- src/spikes/WebServer/YesodServer.hs
+++ src/spikes/WebServer/YesodServer.hs
@@ -9,12 +9,22 @@ data WebToKindleServer = WebToKindleServer
 instance Yesod WebToKindleServer 
 
 mkYesod "WebToKindleServer" [parseRoutes|
-    / IndexR GET
+    /               IndexR GET
+    /?String        TestR GET
+    /stylesheets/#String StyleR GET 
 |]
 
 getIndexR = do
     content <- liftIO $ readFile "public/index.html"    
+    return $ RepHtml $ toContent content
 
-    return $ RepPlain $ toContent $ show content
+getStyleR fileName = do
+    content <- liftIO $ readFile $ "public/stylesheets/" ++ fileName
+    return $ RepPlain $ toContent content
+
+getTestR qry = do
+    liftIO $ print qry
+    content <- liftIO $ readFile "public/index.html"    
+    return $ RepHtml $ toContent content
 
 main = warpDebug 3000 WebToKindleServer 
