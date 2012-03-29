801128cb1a2377878b7bdc0d6a05e53ab6719bce
Thorsten Lorenz <thlorenz@gmx.de>
2012-03-29 08:23:16 -0400

    updating install

--- Install.sh
+++ Install.sh
@@ -1,83 +1,131 @@
 cabal install webtoink/WebToInk.cabal webtoink-converter/webtoink-converter.cabal \ 
-   cprng-aes-0.2.3 \
-   crypto-api-0.9 \
-   crypto-conduit-0.1.3.1 \
-   crypto-pubkey-types-0.1.1 \
-   cryptocipher-0.3.0 \
-   cryptohash-0.7.4 \
-   css-text-0.1.1 \
-   data-default-0.3.0 \
-   dlist-0.5 \
-   email-validate-0.2.8 \
-   entropy-0.2.1 \
-   enumerator-0.4.18 \
-   failure-0.2.0 \
-   fast-logger-0.0.2 \
-   file-embed-0.0.4.1 \
-   hamlet-0.10.9.1 \
-   hashable-1.1.2.3 \
-   hjsmin-0.0.15 \
-   http-conduit-1.2.6 \
-   http-date-0.0.2 \
-   http-enumerator-0.7.3 \
-   http-types-0.6.9 \
-   language-javascript-0.4.10 \
-   largeword-1.0.1 \
-   lifted-base-0.1.0.3 \
-   mime-mail-0.4.1.1 \
-   monad-control-0.3.1 \
-   path-pieces-0.1.0 \
-   persistent-0.8.0.2 \
-   persistent-sqlite-0.8.0 \
-   persistent-template-0.8.2 \
-   pool-conduit-0.0.0.1 \
-   primitive-0.4.1 \
-   pureMD5-2.1.0.3 \
-   pwstore-fast-2.2 \
-   ranges-0.2.4 \
-   resource-pool-0.2.1.0 \
-   safe-0.3.3 \
-   semigroups-0.8 \
-   shakespeare-0.11 \
-   shakespeare-css-0.10.8 \
-   shakespeare-i18n-0.0.2.1 \
-   shakespeare-js-0.11.2 \
-   shakespeare-text-0.11 \
-   simple-sendfile-0.2.1 \
-   skein-0.1.0.5 \
-   socks-0.4.1 \
-   system-filepath-0.4.6 \
-   tagged-0.2.3.1 \
-   tagsoup-0.12.6 \
-   temporary-1.1.2.3 \
-   text-0.11.1.13 \
-   tls-0.9.0 \
-   tls-extra-0.4.3 \
-   transformers-base-0.4.1 \
-   unix-compat-0.3.0.1 \
-   unordered-containers-0.2.1.0 \
-   utf8-light-0.4.0.1 \
-   utf8-string-0.3.7 \
-   vault-0.1.0.0 \
-   vector-0.9.1 \
-   wai-1.1.0.1 \
-   wai-app-static-1.1.2 \
-   wai-extra-1.1.0.1 \
-   wai-logger-0.1.4 \
-   warp-1.1.0.1 \
-   web-kindle-0.1.0 \
-   webtoink-converter-0.0.0 \
-   xml-conduit-0.5.4 \
-   xml-types-0.3.1 \
-   xss-sanitize-0.3.1.1 \
-   yaml-0.5.2 \
-   yesod-0.10.2 \
-   yesod-auth-0.8.1.1 \
-   yesod-core-0.10.3 \
-   yesod-default-0.6.1 \
-   yesod-form-0.4.2.1 \
-   yesod-json-0.3.1 \
-   yesod-persistent-0.3.1 \
-   yesod-routes-0.0.1 \
-   yesod-static-0.10.1 \
-   zlib-bindings-0.0.3.2 
+
+   GLUT-2.1.2.1\ 
+   HTTP-4000.1.1\ 
+   HUnit-1.2.2.3\ 
+   OpenGL-2.2.3.0\ 
+   QuickCheck-2.4.1.1\ 
+   array-0.3.0.2\ 
+   base-4.3.1.0\ 
+   bin-package-db-0.0.0.0\ 
+   bytestring-0.9.1.10\ 
+   cgi-3001.1.7.4\ 
+   containers-0.4.0.0\ 
+   deepseq-1.1.0.2\ 
+   directory-1.1.0.0\ 
+   extensible-exceptions-0.1.1.2\ 
+   fgl-5.4.2.3\ 
+   filepath-1.2.0.0\ 
+   ghc-7.0.3\ 
+   ghc-binary-0.5.0.2\ 
+   ghc-prim-0.2.0.0\ 
+   haskell-src-1.0.1.4\ 
+   haskell2010-1.0.0.0\ 
+   haskell98-1.1.0.1\ 
+   hpc-0.5.0.6\ 
+   html-1.0.1.2\ 
+   integer-gmp-0.2.0.3\ 
+   mtl-2.0.1.0\ 
+   network-2.3.0.2\ 
+   old-locale-1.0.0.2\ 
+   old-time-1.0.0.6\ 
+   parallel-3.1.0.1\ 
+   parsec-3.1.1\ 
+   pretty-1.0.1.2\ 
+   process-1.0.1.5\ 
+   random-1.0.0.3\ 
+   regex-base-0.93.2\ 
+   regex-compat-0.93.1\ 
+   regex-posix-0.94.4\ 
+   rts-1.0\ 
+   stm-2.2.0.1\ 
+   syb-0.3\ 
+   template-haskell-2.5.0.0\ 
+   text-0.11.0.6\ 
+   time-1.2.0.3\ 
+   transformers-0.2.2.0\ 
+   unix-2.4.2.0\ 
+   xhtml-3000.2.0.1\ 
+   zlib-0.5.3.1\ 
+cprng-aes-0.2.3 \
+crypto-api-0.9 \
+crypto-conduit-0.1.3.1 \
+crypto-pubkey-types-0.1.1 \
+cryptocipher-0.3.0 \
+cryptohash-0.7.4 \
+css-text-0.1.1 \
+data-default-0.3.0 \
+dlist-0.5 \
+email-validate-0.2.8 \
+entropy-0.2.1 \
+enumerator-0.4.18 \
+failure-0.2.0 \
+fast-logger-0.0.2 \
+file-embed-0.0.4.1 \
+hamlet-0.10.9.1 \
+hashable-1.1.2.3 \
+hjsmin-0.0.15 \
+http-conduit-1.2.6 \
+http-date-0.0.2 \
+http-enumerator-0.7.3 \
+http-types-0.6.9 \
+language-javascript-0.4.10 \
+largeword-1.0.1 \
+lifted-base-0.1.0.3 \
+mime-mail-0.4.1.1 \
+monad-control-0.3.1 \
+path-pieces-0.1.0 \
+persistent-0.8.0.2 \
+persistent-sqlite-0.8.0 \
+persistent-template-0.8.2 \
+pool-conduit-0.0.0.1 \
+primitive-0.4.1 \
+pureMD5-2.1.0.3 \
+pwstore-fast-2.2 \
+ranges-0.2.4 \
+resource-pool-0.2.1.0 \
+safe-0.3.3 \
+semigroups-0.8 \
+shakespeare-0.11 \
+shakespeare-css-0.10.8 \
+shakespeare-i18n-0.0.2.1 \
+shakespeare-js-0.11.2 \
+shakespeare-text-0.11 \
+simple-sendfile-0.2.1 \
+skein-0.1.0.5 \
+socks-0.4.1 \
+system-filepath-0.4.6 \
+tagged-0.2.3.1 \
+tagsoup-0.12.6 \
+temporary-1.1.2.3 \
+text-0.11.1.13 \
+tls-0.9.0 \
+tls-extra-0.4.3 \
+transformers-base-0.4.1 \
+unix-compat-0.3.0.1 \
+unordered-containers-0.2.1.0 \
+utf8-light-0.4.0.1 \
+utf8-string-0.3.7 \
+vault-0.1.0.0 \
+vector-0.9.1 \
+wai-1.1.0.1 \
+wai-app-static-1.1.2 \
+wai-extra-1.1.0.1 \
+wai-logger-0.1.4 \
+warp-1.1.0.1 \
+web-kindle-0.1.0 \
+webtoink-converter-0.0.0 \
+xml-conduit-0.5.4 \
+xml-types-0.3.1 \
+xss-sanitize-0.3.1.1 \
+yaml-0.5.2 \
+yesod-0.10.2 \
+yesod-auth-0.8.1.1 \
+yesod-core-0.10.3 \
+yesod-default-0.6.1 \
+yesod-form-0.4.2.1 \
+yesod-json-0.3.1 \
+yesod-persistent-0.3.1 \
+yesod-routes-0.0.1 \
+yesod-static-0.10.1 \
+zlib-bindings-0.0.3.2 
