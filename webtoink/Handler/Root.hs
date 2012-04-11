8687af082038a5b3c73d37b421400561e961db61
Thorsten Lorenz <thlorenz@gmx.de>
2012-04-11 08:13:30 -0400

    spinner works now

--- webtoink/Handler/Root.hs
+++ webtoink/Handler/Root.hs
@@ -11,7 +11,10 @@ import Import
 -- inclined, or create a single monolithic file.
 getRootR :: Handler RepHtml
 getRootR = defaultLayout $ do
-        h2id <- lift newIdent
-        setTitle "WebToInk homepage"
-        $(widgetFile "homepage")
-        addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
+    h2id <- lift newIdent
+    setTitle "WebToInk homepage"
+    $(widgetFile "homepage")
+    addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
+    -- TODO: somewhat hacky to abuse addScriptRemote for a local script, but addScript and addJavaScript
+    -- both take a Route instead of just a String (path
+    addScriptRemote "static/js/spin.min.js"
