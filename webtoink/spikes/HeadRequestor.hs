735e08c0a9036f3eb1ba03128a744f1193993784
Thorsten Lorenz <thlorenz@gmx.de>
2012-04-11 06:54:30 -0400

    removing old spikes

--- webtoink/spikes/HeadRequestor.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Network.HTTP.Enumerator
-
-main = do
-    req <- parseUrl "http://learnyouahaskell.com/chapters"
-    res <- withManager $ httpLbs req { method = "HEAD" }
-    print res
